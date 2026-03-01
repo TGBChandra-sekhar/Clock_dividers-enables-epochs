53e05a02389d9d58aa4056c68db566565c47cdc6
Tanuku Gnanendra Bala Chandra Sekhar <23a95a0426@aec.edu.in>
2026-03-01 22:43:08 +0530

    Format parameter declaration for consistency

--- clk_div_n_odd.v
+++ clk_div_n_odd.v
@@ -7,9 +7,10 @@ module clk_div_n_odd (
     input clk,rst;
     output reg clk_out;
     
-    parameter integer N = 5;
+    parameter integer N   = 5;
     localparam HIGH_COUNT = (N + 1) / 2;
     localparam LOW_COUNT  = (N - 1) / 2;
+        
     localparam integer COUNT_W  = $clog2(HIGH_COUNT);
     
     reg [COUNT_W-1:0]count;
@@ -17,7 +18,7 @@ module clk_div_n_odd (
     always @(posedge clk or posedge rst) begin
         if(rst) begin
             clk_out <= 1'b0;
-            count <= 'b0;
+            count   <= 'b0;
         end
         else begin
         if(clk_out) begin
