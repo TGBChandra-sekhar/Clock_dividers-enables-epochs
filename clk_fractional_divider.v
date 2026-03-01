889632d69de42eccd87f9e1252df4ae4d91b8e5e
Tanuku Gnanendra Bala Chandra Sekhar <23a95a0426@aec.edu.in>
2026-03-01 22:03:10 +0530

    Create clk_fractional_divider.v

---
+++ clk_fractional_divider.v
@@ -0,0 +1,28 @@
+
+// DDS Method
+module fractional_divider (
+        clk,
+        rst,
+        fcw,
+        clk_out  
+    );
+
+    input  clk;
+    input  rst;
+    input  [31:0] fcw; // Frequency Control Word
+    output reg clk_out;
+    
+    reg [31:0] phase_acc;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            phase_acc <= 32'd0;
+            clk_out   <= 1'b0;
+        end 
+        else begin
+            phase_acc <= phase_acc + fcw;
+            clk_out   <= phase_acc[31];
+        end
+    end
+
+endmodule
